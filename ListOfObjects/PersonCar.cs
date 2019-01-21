191466bef29dbcbac9a5fef15ecb06dba8e1aac0
nandress09 <andres23@mail.nmc.edu>
2019-01-20 20:24:41 -0500

    Add project files.

---
+++ ListOfObjects/PersonCar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ListOfObjects
+{
+    class PersonCar
+    {
+        public enum MakeName
+        {
+            NONE,
+            DODGE,
+            FORD,
+            CHEVY,
+
+        }
+
+        public bool IsHardTop { get; internal set; }
+        public double Engine { get; internal set; }
+        public int Year { get; internal set; }
+        public string Model { get; internal set; }
+        public MakeName Make { get; internal set; }
+        
+    }
+}
